ba2f0153dcb1a8c8c0a5b4b14f949f70b96694be
westjac <westjacob1@gmail.com>
2016-12-26 13:54:28 -0700

    Initial Commit

---
+++ Devslopes Social/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Devslopes Social
+//
+//  Created by Jacob West on 12/26/16.
+//  Copyright © 2016 Jacob West. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
